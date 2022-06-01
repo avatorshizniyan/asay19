70666b67ba403abf8d003c0416bbd166e06a60aa
avatorshizniyan <104760992+avatorshizniyan@users.noreply.github.com>
2022-06-01 17:10:44 +0430

    Add files via upload

---
+++ Assets/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        Vector2 move = new Vector2(x, 0);
+        transform.Translate(move * speed *Time.deltaTime);
+    }
+}
